3b6854aba0ddd695924a1bd94f10bfb2864eb198
root <root@HYS12904CVWHR2.localdomain>
2026-01-07 18:47:57 +0530

    initial code push

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Endpoint 1";
+    }
+
+    @GetMapping("/status")
+    public String status() {
+        return "Application is running - Endpoint 2";
+    }
+}
